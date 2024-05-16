583770b5811cbddb0493ab97195a718d98852a8c
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-16 17:16:25 +0900

    build: 웹 배포

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/lib/index.ts'),
-      name: 'index',
-      fileName: 'index',
-    },
-    rollupOptions: {
-      external: ['react'],
-      output: {
-        globals: {
-          react: 'React',
-        },
-      },
-    },
-    commonjsOptions: {
-      esmExternals: ['react'],
-    },
-  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
